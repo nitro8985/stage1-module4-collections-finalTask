f949ebf304486524df7ebfb4a44dd1ad558e7f3c
nitro <104995604+nitro8985@users.noreply.github.com>
2023-10-08 11:23:49 +0600

    Done

--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,21 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+
+        Map<Integer, Set<String>> resultMap = new HashMap<>();
+
+        for (Map.Entry<String, Integer> entry : sourceMap.entrySet()) {
+            String key = entry.getKey();
+            int keyLength = key.length();
+            resultMap.computeIfAbsent(keyLength, k -> new HashSet<>()).add(key);
+        }
+
+        return resultMap;
     }
 }
